34d15f289440b1a1c8bfe24e13cb5b87ec32066b
Nishil Anand <nishilanand21@gmail.com>
2022-05-24 19:06:44 -0700

    Setup

---
+++ PeriodicTable/src/DrawingSurface.java
@@ -0,0 +1,30 @@
+import processing.core.PApplet;
+
+/**
+ * @author nishoof
+ */
+public class DrawingSurface extends PApplet {
+	
+	/**
+	 * Constructs a new DrawingSurface
+	 */
+	public DrawingSurface() {
+		
+	}
+	
+	
+	/**
+	 * Called once when the program starts
+	 */
+	public void setup() {
+		
+	}
+	
+	/**
+	 * Draws to the window
+	 */
+	public void draw() {
+		
+	}
+	
+}
---
+++ PeriodicTable/src/Main.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+		
+		window.setSize(1050, 600);
+		window.setLocation(20, 20);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+		
+		window.setVisible(true);
+		
+		canvas.requestFocus();
+		
+	}
+	
+}
