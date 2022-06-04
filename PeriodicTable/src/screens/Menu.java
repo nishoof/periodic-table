67c1d540843103a8da1ffc33e0f347dedffb4bb2
Nishil Anand <nishilanand21@gmail.com>
2022-06-03 22:59:21 -0700

    Redoing Menu part 1

--- PeriodicTable/src/screens/Menu.java
+++ PeriodicTable/src/screens/Menu.java
@@ -1,9 +1,7 @@
 package screens;
 
 import core.DrawingSurface;
-import java.awt.Point;
-import java.awt.Rectangle;
-import processing.core.PConstants;
+import nish.processing.Button;
 
 
 /**
@@ -13,58 +11,25 @@ import processing.core.PConstants;
  */
 public class Menu extends Screen {
 
-    private Rectangle button;
+    private Button button;
 
     /**
-     * Constructs a new Menu Screen
+     * Constructs a new Menu Screen.
      * 
-     * @param surface the DrawingSurface to use. Should not be null.
+     * @param surface the DrawingSurface to use. Should not be null
      */
     public Menu(DrawingSurface surface) {
-        
         super(surface);
-
-        button = new Rectangle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 150, 50);
-        
     }
 
 
     /**
-     * Draws this Menu
+     * Draws this Menu. Called every frame.
      */
     public void draw() {
 
-        surface.strokeWeight(1);
-        surface.fill(255);
-        surface.rectMode(PConstants.CENTER);
-        surface.textAlign(PConstants.CENTER, PConstants.CENTER);
-
-        surface.background(255);;
-
-        surface.rect(button.x, button.y, button.width, button.height);
-
-        surface.fill(0);
-        surface.textSize(17);
-        surface.text("View Periodic Table", button.x, button.y);
+        
 
     }
-
-    /**
-     * Called when the mouse is pressed.
-     * Used for detecting if the user clicked on a button.
-     */
-    public void mousePressed() {
-
-        int x = surface.mouseX;
-        int y = surface.mouseY;
-
-        if (button.contains(new Point(x + button.width/2, y + button.height/2))) {              // the +/- for button width/height is because the rectangle is drawn in the center
-            surface.switchScreen(1);
-        }
-
-
-    }
-
-
 
 }
